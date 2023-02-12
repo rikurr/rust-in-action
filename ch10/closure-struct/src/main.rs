0b0025b62b9b8fa7f4c5f5d0679ba7922b044271
oya riku <56904584+rikurr@users.noreply.github.com>
2023-02-12 22:06:40 +0900

    Add:ch10.3

---
+++ ch10/closure-struct/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let a = 20;
+
+    let add_to_a = |b| a + b;
+    add_to_a == ();
+}
---
+++ ch10/shared-pause/src/main.rs
@@ -0,0 +1,15 @@
+use std::{thread, time};
+
+fn main() {
+    let pause = time::Duration::from_millis(20);
+    let handle1 = thread::spawn(move || {
+        thread::sleep(pause);
+    });
+
+    let handle2 = thread::spawn(move || {
+        thread::sleep(pause);
+    });
+
+    handle1.join();
+    handle2.join();
+}
